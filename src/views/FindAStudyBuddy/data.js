329c4370f97b4e74ed152569950cf79284228a47
bpio12 <bpio@wellesley.edu>
2019-09-15 10:28:16 -0400

    ?

--- src/views/FindAStudyBuddy/data.js
+++ src/views/FindAStudyBuddy/data.js
@@ -5,13 +5,8 @@ export default [
     id: uuid(),
     title: 'Chantal Valdivia',
     description:'MIT 2022. CS Major, Musical Theatre minor',
-<<<<<<< HEAD
     imageUrl: '/images/avatars/chantal.jpg',
     totalDownloads: '594',
-=======
-    imageUrl: '/images/products/product_1.jpeg',
-    totalDownloads: '3',
->>>>>>> 81ec8a404ea81a32d6965e4d8afb30639935dd6f
     updatedAt: '27/03/2019'
   },
   {
@@ -19,13 +14,8 @@ export default [
     title: 'Mark Zuk',
     description:
       'MIT 2020. CS Major',
-<<<<<<< HEAD
     imageUrl: '/images/avatars/noah.jpg',
     totalDownloads: '406',
-=======
-    imageUrl: '/images/products/machine learning.png',
-    totalDownloads: '8',
->>>>>>> 81ec8a404ea81a32d6965e4d8afb30639935dd6f
     createdAt: '04/04/2019'
   },
   {
@@ -33,28 +23,17 @@ export default [
     title: 'Jen Howe',
     description:
       'Wellesley 2022. MAS major',
-<<<<<<< HEAD
     imageUrl: '/images/avatars/rebekah.jpg',
     totalDownloads: '835',
-=======
-    imageUrl: '/images/products/soc.jpg',
-    totalDownloads: '2',
->>>>>>> 81ec8a404ea81a32d6965e4d8afb30639935dd6f
     createdAt: '04/04/2019'
   },
   {
     id: uuid(),
     title: "Xavier O'Connor",
     description:
-<<<<<<< HEAD
       'MIT 2023. Econ major, anthro minor',
     imageUrl: '/images/avatars/sean.jpg',
     totalDownloads: '625',
-=======
-      ' ',
-    imageUrl: '/images/products/musical theatre.jpg',
-    totalDownloads: '5',
->>>>>>> 81ec8a404ea81a32d6965e4d8afb30639935dd6f
     createdAt: '31/03/2019'
   },
   {
@@ -62,13 +41,8 @@ export default [
     title: 'Gabriela Garcia',
     description:
       'Wellesley 2022. Linguistics major',
-<<<<<<< HEAD
     imageUrl: '/images/avatars/gabby.jpg',
     totalDownloads: '857',
-=======
-    imageUrl: '/images/products/genes.jpg',
-    totalDownloads: '9',
->>>>>>> 81ec8a404ea81a32d6965e4d8afb30639935dd6f
     createdAt: '03/04/2019'
   }
   
