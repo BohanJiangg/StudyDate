7e242d66b40ea7bd7d4ccf970aa78b1ded57bb0d
tiffanyhuynh <tiffanyhuynh@Tiffanys-MacBook-Pro-2.local>
2019-09-15 11:18:58 -0400

    changed enrolled to members

--- src/views/Groups/components/ProductCard/ProductCard.js
+++ src/views/Groups/components/ProductCard/ProductCard.js
@@ -98,7 +98,7 @@ const ProductCard = props => {
               display="inline"
               variant="body2"
             >
-              {product.totalDownloads} Enrolled
+              {product.totalDownloads} Members
             </Typography>
           </Grid>
         </Grid>
