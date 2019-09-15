47901fcfddc7346bad6514f0c0f90cfa6f36f402
tiffanyhuynh <tiffanyhuynh@Tiffanys-MacBook-Pro-2.local>
2019-09-14 20:09:42 -0400

    updated profile

--- src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
+++ src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
@@ -40,14 +40,14 @@ const ProductsToolbar = props => {
     >
       <div className={classes.row}>
         <span className={classes.spacer} />
-        <Button className={classes.importButton}>Import</Button>
+        {/* <Button className={classes.importButton}>Import</Button>
         <Button className={classes.exportButton}>Export</Button>
         <Button
           color="primary"
           variant="contained"
         >
           Add product
-        </Button>
+        </Button> */}
       </div>
       <div className={classes.row}>
         <SearchInput
